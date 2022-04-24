14f974d1a16e15068b16f22f575bfc54d630d623
Ayrton <devayrtonsbarreto@gmail.com>
2022-04-24 11:49:51 -0300

    imagens com hierarquia das classes responsáveis por leitura e escrita

--- src/teste/leitura/TesteLeitura.java
+++ src/teste/leitura/TesteLeitura.java
@@ -6,9 +6,9 @@ public class TesteLeitura {
     public static void main(String[] args) throws IOException {
 
         //Fluxo de Entrada com Arquivo
-        FileInputStream fis = new FileInputStream("lorem.txt"); //retorna bytes
+        InputStream fis = new FileInputStream("lorem.txt"); //retorna bytes
 
-        InputStreamReader isr = new InputStreamReader(fis);  //bytes -> char
+        Reader isr = new InputStreamReader(fis);  //bytes -> char
 
         BufferedReader br = new BufferedReader(isr);
         //br.readLine(); char -> linha
